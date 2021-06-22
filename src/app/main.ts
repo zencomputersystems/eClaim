6e0d8d42bbdbb9086c97228d686162efa2ad98d7
Mohd Khairulnizam <steelburn@gmail.com>
2021-06-22 17:09:57 +0800

    Updated ENV

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,5 +3,5 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 enableProdMode();
-console.log("eClaim Web - release date: 24 February 2020");
+console.log("eClaim Web - release date: 22 June 2021");
 platformBrowserDynamic().bootstrapModule(AppModule);
