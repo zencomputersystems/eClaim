b2b1b193c3cb30bbbe82265b2e065d56da36a5f6
Mohd Khairulnizam <steelburn@gmail.com>
2021-06-23 12:09:56 +0800

    Added run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-EMAIL_KEY='42629d7617a5e3f440fe039af8775fcdc5c5a26e65e05223970c920a0ad7d8e7'
-DREAMFACTORY_API_KEY='cb82c1df0ba653578081b3b58179158594b3b8f29c4ee1050fda1b7bd91c3881'
-AD_URL='https://auth.zen.com.my'
-DREAMFACTORY_INSTANCE_URL='https://api.zen.com.my'
 ionic serve --external -c --prod --no-open --no-livereload
